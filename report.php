4f3804cf70c928fb58aa7c08046d74675a82edb3
Nasir Khan Saikat <nasir8891@gmail.com>
2013-10-31 23:30:52 +0600

    updated report listing

---
+++ report.php
@@ -0,0 +1,141 @@
+<?php
+require_once 'configuration.php';
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"  lang="en"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Organization Registry</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+        <div class="container">
+
+            <!-- Main jumbotron for a primary marketing message or call to action -->
+            <div class="jumbotron">
+                <h1>Organization Registry</h1>
+                <p>
+                    Government of People's Republic of Bangladesh <br />
+                    Directorate General of Health Services
+                </p>
+            </div>
+            <div class="navbar navbar-inverse navbar-default">
+                <!--<div class="container">-->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">DGHS</a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="#">Apply for registration</a></li>
+                        <li class="active"><a href="report.php">Report Dashboard</a></li>
+                        <li><a href="search.php">Search</a></li>
+                    </ul>
+                    <form name="search-form" class="navbar-form navbar-right" action="search.php" method="post">
+                        <div class="form-group">
+                            <input name="query" type="text" placeholder="Enter keyward" class="form-control">
+                        </div>                       
+                        <button type="submit" class="btn btn-success">Search</button>
+                    </form>
+                </div><!--/.navbar-collapse -->
+                <!--</div>-->
+            </div>
+        </div>
+
+        <div class="container">
+            <!-- Example row of columns -->
+            <div class="row">                
+
+                <div class="col-md-12">
+                    <h2>Report Dashboard</h2>
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <td><strong>Report name</strong></td>
+                                <td><strong>Description</strong></td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><a href="report_org_list.php">Organization list</a></td>
+                                <td>
+
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+
+            <hr>
+
+            <footer>
+                <p>&copy; Company 2013</p>
+            </footer>
+        </div> <!-- /container -->        
+
+
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+
+        <!--        
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+        -->
+
+        <script type="text/javascript" src="library/jstree-bootstrap-theme-master/jquery.js"></script>
+        <script type="text/javascript" src="library/jstree-bootstrap-theme-master/jquery.cookie.js"></script>
+        <script type="text/javascript" src="library/jstree-bootstrap-theme-master/jquery.hotkeys.js"></script>
+        <script type="text/javascript" src="library/jstree-bootstrap-theme-master/jquery.jstree.js"></script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <script type="text/javascript" class="source below">
+            $(function() {
+                $("#org_list").jstree({
+                    "plugins": ["themes", "html_data", "ui", "crrm", "hotkeys"],
+                    "core": {
+                        "animation": 100
+                    },
+                    "themes": {
+                        "theme": "proton"
+                    },
+                })
+            });
+        </script>
+
+        <!--
+        <script>
+            var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']];
+            (function(d, t) {
+                var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+                g.src = '//www.google-analytics.com/ga.js';
+                s.parentNode.insertBefore(g, s)
+            }(document, 'script'));
+        </script>
+        -->
+    </body>
+</html>
