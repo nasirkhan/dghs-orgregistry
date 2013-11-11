43fe6d9233003da15be949d6289732a0c980117f
Nasir Khan Saikat <nasir8891@gmail.com>
2013-11-11 20:23:20 +0600

    fix

--- include/header_page_header.php
+++ include/header_page_header.php
@@ -1,9 +1,9 @@
 <div class="jumbotron">
     <div class="row">
-        <div class="col-md-2">
+        <div class="col-xs-5 col-sm-5 col-md-2 col-lg-2">
             <img src="img/bangladesh_govt_logo.png" class="img-responsive" alt="Bangladesh Govt. Logo">
         </div>
-        <div class="col-md-10">
+        <div class="col-xs-7 col-sm-7 col-md-10 col-lg-10">
             <h1>Organization Registry</h1>
             <p>
                 Government of People's Republic of Bangladesh <br />
