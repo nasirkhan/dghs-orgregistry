fd749aae73cc40d1cfb23355810e81ae27312104
nasirkhan <nasir8891@gmail.com>
2013-11-19 12:19:48 +0600

    header fix

--- include/header_page_header.php
+++ include/header_page_header.php
@@ -1,14 +1,17 @@
 <div class="jumbotron">
     <div class="row">
-        <div class="col-xs-5 col-sm-5 col-md-2 col-lg-2">
-            <img src="img/bangladesh_govt_logo.png" class="img-responsive" alt="Bangladesh Govt. Logo">
-        </div>
-        <div class="col-xs-7 col-sm-7 col-md-10 col-lg-10">
-            <h1>Organization Registry</h1>
-            <p>
-                Government of People's Republic of Bangladesh <br />
-                Ministry of Health and Family Welfare
-            </p>
+        <div class="col-xs-7 col-sm-7 col-md-11 col-lg-10">
+            <div class="logo pull-left">
+                <img src="img/bangladesh_govt_logo.png" class="img-responsive" alt="Bangladesh Govt. Logo" style="width: 100px;">
+            </div>
+            <div class="pull-left">
+                <h1>Organization Registry</h1>
+                <p>
+                    Government of People's Republic of Bangladesh <br />
+                    Ministry of Health and Family Welfare
+                </p>
+            </div>
+            
         </div>
     </div>
 </div>
